d1443d3be2d31efac00e71841e3cadcc1e30cd83
David Zúñiga <davidzunigaortiz92@gmail.com>
2017-01-12 15:40:56 -0500

    Add files via upload

---
+++ subir.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class subir : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    [System.Web.Services.WebMethod]
+    public static int guardarImagen()
+    {
+        var request = HttpContext.Current.Request;
+        if (request.Files.Count > 0)
+        {
+            foreach (string file in request.Files)
+            {
+                var postedFile = request.Files[file];
+                var filePath = HttpContext.Current.Server.MapPath(string.Format("~/productos/{0}", postedFile.FileName));
+                postedFile.SaveAs(filePath);
+            }
+            return 1;
+        }
+        else
+            return 0;
+
+    }
+
+}
---
+++ transacciones.aspx.cs
@@ -0,0 +1,53 @@
+﻿using Proyecto_Integrador_Elect.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class transacciones : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string[] aux = Request.UrlReferrer.ToString().Split('/');
+        string proviene = aux[aux.GetUpperBound(0)];
+        bool contiene = Request.UrlReferrer.ToString().Contains('?');
+        producto unProducto = new producto();
+        if (contiene)
+        {
+            aux = proviene.Split('?');
+            proviene = aux[0];
+        }
+        if (proviene == "regristroproducto.html")
+        {
+
+            HttpFileCollection files = Request.Files;
+            string nombreImagen = "";
+            int pos_ult_puntos = 0;
+                int long_nombre = 0;
+                
+                string ext = "";
+            for (int i = 0; i < files.Count; i++)
+            {
+                
+                HttpPostedFile f = files[0];
+                nombreImagen= f.FileName;
+                f = files[i];
+                f.SaveAs(Server.MapPath(".") + @"\productos\" + f.FileName);
+                pos_ult_puntos=f.FileName.LastIndexOf(".");
+                long_nombre=f.FileName.Length;
+                int cant_caract_extraer = long_nombre - pos_ult_puntos;
+                ext=f.FileName.Substring(pos_ult_puntos, cant_caract_extraer);
+            }
+
+             
+            //unProducto.agregarProducto(Request.Form["txtNombre"].ToString(), Request.Form["txtDescripcion"].ToString(), Convert.ToInt32(Request.Form["txtStock"].ToString()), Convert.ToInt32(Request.Form["opCategoria"].ToString()), Convert.ToInt32(Request.Form["opMarcas"].ToString()), Convert.ToDecimal(Request.Form["txtPrecio"].ToString()));
+            unProducto.agregarImagen(nombreImagen, ext);
+            Response.Redirect(proviene, false);
+            
+ 
+        }
+
+    }
+}
